b60fc33fb23c31ac08b11e630921ce3d6eae4da7
e-soysal <emilie.rosenlund@gmail.com>
2025-12-21 22:43:36 +0300

    Add environment file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     # run_save_table_to_csv()
     # run_print_tournament_table()
     # run_print_player_table()
+    print("This is the end of the main function.")
     pass
 
 if __name__ == "__main__":
