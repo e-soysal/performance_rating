2494da9db247072582af8c160bd0745c6f1238cb
e-soysal <emilie.rosenlund@gmail.com>
2025-12-24 10:08:47 +0300

    Two view functionality added

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ from io import StringIO
 st.set_page_config(page_title="PGN Player Viewer")
 
 st.title("Performance Rating Calculator")
+st.write("This app allows you to upload a PGN file and view player performance rating calculated by different methods.")
+
 # Upload PGN file
 uploaded_file = st.file_uploader("Upload a PGN file", type=["pgn"])
 
@@ -16,30 +18,50 @@ if uploaded_file is not None:
     
     df2 = functions.games_to_player_rows(df1)
     
+    df3 = functions.build_player_opponent_elo_table_from_df(df2)
+    
     st.write("PGN file parsed successfully.")
+
+    col1, col2 = st.columns(2)
     
-    # Test player detail function
-    players = df2["Player"].unique()
+    with col1:
+        if st.button("Tournament view"):
+            st.session_state.view = "tournament"
 
-    # Dropdown with player names
-    selected_player = st.selectbox(
-        "Select a player to view performance:",
-        sorted(players)
-    )
+    st.divider()
+    with col2:
+    if st.button("Player view"):
+        st.session_state.view = "player"
 
-    # 4️⃣ Display something related to the player
-    df4 = df2.copy() #Get a list of all the games
-    df4 = df4[df4["Player"] == selected_player] #Filter for the test player
-    test_player = functions.player_detail(name = selected_player, player_games = df4) #create player detail
-    st.write("Player details retrieved for player:", test_player.player_name)
-    st.write("Elo rating:", test_player.player_rating, "Score:", test_player.score, "Rounds played:", test_player.player_rounds)
-    
-    st.write(test_player.player_games)
+    if st.session_state.view == "tournament":
+        st.header("Tournament view")
+        st.write(df3)
 
-    #Calculate the performance rating for the test player
-    test_player.calculate_performance_rating(boundary_value= 0.01)
-    st.write("Calculated performance - average method:", test_player.performance_average)
-    st.write("Calculated performance - minimisation method:", test_player.performance)
+    elif st.session_state.view == "player":
+        st.header("Player view")
+        st.write("Show player-level data here")
+
+        # Test player detail function
+        players = df2["Player"].unique()
+
+        # Dropdown with player names
+        selected_player = st.selectbox(
+            "Select a player to view performance:",
+            sorted(players)
+        )
+
+        # Display something related to the player
+        df4 = df2.copy() #Get a list of all the games
+        df4 = df4[df4["Player"] == selected_player] #Filter for the test player
+        test_player = functions.player_detail(name = selected_player, player_games = df4) #create player detail
+        st.write("Player details retrieved for player:", test_player.player_name)
+        st.write("Elo rating:", test_player.player_rating, "Score:", test_player.score, "Rounds played:", test_player.player_rounds)
+        st.write(test_player.player_games)
+
+        #Calculate the performance rating for the test player
+        test_player.calculate_performance_rating(boundary_value= 0.01)
+        st.write("Calculated performance - average method:", test_player.performance_average)
+        st.write("Calculated performance - minimisation method:", test_player.performance)
 
 else:
     st.warning("Please upload a PGN file to proceed.")
